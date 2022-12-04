1bb3a1a8e12944582c515b1eb62fb3f6876938b1
Mitchell Foote <mcfoote321@gmail.com>
2022-12-03 23:51:45 -0500

    work on models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,28 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.Decimal(12, 2),
+      allowNull: false,
+    },
+    stock: {
+      type: DataTpes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
   {
     sequelize,
